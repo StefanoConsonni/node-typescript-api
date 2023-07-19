f094e97c6742d1f3162d380eee07c87b2297986f
Stefano Consonni <stefanoconsonni@ymail.com>
2023-07-19 11:38:52 +0200

    create users schema

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
-
-import router from "./router";
 import mongoose from "mongoose";
+import router from "./router";
 
 const app = express();
 
@@ -21,15 +20,14 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
+const PORT = process.env.PORT || 8080;
 
-server.listen(8080, () => {
-  console.log("Server running on http://localhost:8080/");
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/`);
 });
 
-const MONGO_URL = ""; // DB URI
-
 mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL);
+mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.on("error", (error: Error) => console.log(error));
 
 app.use("/", router());
