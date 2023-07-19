6e8bfbae61ead679633cd2799b4318b1af8bf1dd
Stefano Consonni <stefanoconsonni@ymail.com>
2023-07-19 12:07:33 +0200

    create users router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,13 @@
 import express from "express";
-import http from "http";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import compression from "compression";
-import cors from "cors";
 
-import router from "./router";
-import mongoose from "mongoose";
+import authentication from "./authentication";
+import users from "./users";
 
-const app = express();
+const router = express.Router();
 
-app.use(
-  cors({
-    credentials: true,
-  })
-);
+export default (): express.Router => {
+  authentication(router);
+  users(router);
 
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json());
-
-const server = http.createServer(app);
-
-server.listen(8080, () => {
-  console.log("Server running on http://localhost:8080/");
-});
-
-const MONGO_URL = ""; // DB URI
-
-mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL);
-mongoose.connection.on("error", (error: Error) => console.log(error));
-
-app.use("/", router());
+  return router;
+};
